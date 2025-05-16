754679231fc52474160fae15cf013df6a2a81e60
Soujanya Maharudra Bailawad <soujanyabailawad@gmail.com>
2025-05-16 21:38:27 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const main = async ()=>{
+    const User = await prisma.User.create({
+        data:{
+            name:"souju",
+            email:"souju@gmail.com",
+        }
+    })
+    console.log(User)
+}
+main()
+.catch((e) => console.error(e))
+.finally(async ()=>{
+    await prisma.$disconnect();
+})

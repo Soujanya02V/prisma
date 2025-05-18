c17b3f9d3e53457f2a2b251820dcaab25fb719f2
Soujanya Maharudra Bailawad <soujanyabailawad@gmail.com>
2025-05-18 13:50:33 +0530

    Update app.js
    CRUD operations

--- app.js
+++ app.js
@@ -11,6 +11,37 @@ const main = async ()=>{
         }
     })
     console.log(User)
+      /* const User = await prisma.User.createMany({
+       data:[{
+        name:"alice" , email:"alice@gmail.com"
+       },
+       {
+         name:"bob", email:"bob@gmail.com"
+       },
+    ],
+})
+    console.log(User);*/
+    //only unique fields are used mentioned in the schema.prisma
+    const User = await prisma.User.findMany()
+    const user = await prisma.User.findUnique({
+        where:{id:3},
+    })
+    console.log(User)
+    console.log(user)
+
+    //updating
+    const upuse = await prisma.User.update({
+        where:{id: 3},
+        data:{name: "tony stark"},
+    })
+    console.log(user)
+    //when using updateMany dont use unique attributes
+    //deleting
+    const del = await prisma.User.delete({
+        where:{id:2},
+    })
+    console.log(user)
+    
 }
 main()
 .catch((e) => console.error(e))
